5b32cbcf829f1bf1ba06734d27d88f8733a1aff4
nageshsrao <nageshsrao@gmail.com>
2021-01-08 15:47:55 +0000

    next instance added

---
+++ instance01.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "beta-instance" {
+  name         = "betainstance01"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+}
